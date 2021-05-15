ae8bd22d6095ad533110e4985a3ba8253bb19184
jessicasewell <sewell.jessica95@gmail.com>
2021-05-15 17:17:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, searchValue) 
+{ 
+   const nameOf =  drivers.filter( drivers => drivers.toUpperCase() === searchValue.toUpperCase()) 
+     return nameOf
+}
+
+function fuzzyMatch(drivers, queryValue) 
+{
+  const driverLetter = drivers.filter(drivers => drivers.startsWith(queryValue))
+  return driverLetter
+}
+
+function matchName(drivers, nameValue)
+{
+const driverObject = drivers.filter(drivers => drivers.name === nameValue)
+return driverObject
+}
